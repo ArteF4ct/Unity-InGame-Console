d385747e29eb615a39ab520c8a65c92d8093b7d9
Musa Özey <musaozey@Musas-MacBook-Pro.local>
2017-07-09 00:14:52 +0300

    Added MCommand attribution

---
+++ Assets/MConsole/MCommandAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MConsole
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class MCommandAttribute : Attribute
+	{
+		public readonly string command;
+
+		public MCommandAttribute(string command)
+		{
+			this.command = command;
+		}
+	}
+}
