a1114dbac07dc82757517c6d1e156aa24a73865a
Musa Özey <musaozey@Musas-MacBook-Pro.local>
2017-07-08 23:19:03 +0300

    Created MConsole file

---
+++ Assets/MConsole/MConsole.cs
@@ -0,0 +1,31 @@
+﻿namespace MConsole
+{
+	public class MConsole
+	{
+		private static MConsole instance;
+
+		public static MConsole Instance() 
+		{
+			if (instance == null)
+			{
+				instance = new MConsole();
+			}
+			return instance;
+		}
+
+		public void Show() 
+		{ 
+			
+		}
+
+		public void Hide()
+		{
+
+		}
+
+		public void ExecuteCommand(string cmd) 
+		{ 
+			
+		}
+	}
+}
