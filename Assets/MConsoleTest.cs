78d5b109fd0983e1ec73fcfd59cf0ae05eaece43
Musa Özey <musaozey@Musas-MacBook-Pro.local>
2017-07-09 00:15:55 +0300

    Added test script

---
+++ Assets/MConsoleTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MConsole;
+
+public class MConsoleTest : MonoBehaviour 
+{
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			MConsole.MConsole.Instance().ExecuteCommand("close_console");
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			MConsole.MConsole.Instance().ExecuteCommand("example gg 1 aaaa");
+		}
+	}
+}
