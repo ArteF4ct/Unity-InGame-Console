f2cb72921b6c4a5a6b483b120f4b4d8af6857d94
Musa Özey <musaozey@Musas-MacBook-Pro.local>
2017-07-09 00:15:16 +0300

    Added test commands

---
+++ Assets/MConsole/MCommands.cs
@@ -0,0 +1,17 @@
+﻿namespace MConsole
+{
+	public partial class MCommands
+	{
+		[MCommand("close_console")]
+		public void CloseConsole(params string[] args) 
+		{
+			MConsole.Instance().Hide();
+		}
+
+		[MCommand("example")]
+		public void ExampleCommand(params string[] args)
+		{
+			UnityEngine.Debug.Log(string.Format("CMD: example / arg0: {0} / arg1: {1} / arg2: {2}", args[0], args[1], args[2]));
+		}
+	}
+}
