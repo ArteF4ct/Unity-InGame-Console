69a6a81e25f69eacc5f62abc083cf0ad75f927d7
Musa Özey <musaozey@Musas-MacBook-Pro.local>
2017-07-09 00:15:40 +0300

    Implemented ExecuteCommand method

--- Assets/MConsole/MConsole.cs
+++ Assets/MConsole/MConsole.cs
@@ -1,8 +1,14 @@
-﻿namespace MConsole
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MConsole
 {
 	public class MConsole
 	{
 		private static MConsole instance;
+		private MCommands mcommands = new MCommands();
 
 		public static MConsole Instance() 
 		{
@@ -20,12 +26,31 @@
 
 		public void Hide()
 		{
-
+			
 		}
 
-		public void ExecuteCommand(string cmd) 
-		{ 
-			
+		public void ExecuteCommand(string cmdString) 
+		{
+			if (string.IsNullOrEmpty(cmdString)) 
+			{ 
+				return;
+			}
+
+			List<string> splittedCmd = new List<string>(cmdString.Split(' '));
+			string cmd = splittedCmd[0];
+			splittedCmd.RemoveAt(0);
+
+			System.Type type = typeof(MCommands);
+			foreach (MethodInfo m in type.GetMethods())
+			{
+				foreach (MCommandAttribute a in m.GetCustomAttributes(typeof(MCommandAttribute), false))
+				{
+					if(string.Equals(cmd, a.command))
+					{
+						m.Invoke(mcommands, new object[] { splittedCmd.ToArray() });
+					}
+				}
+			}
 		}
 	}
 }
