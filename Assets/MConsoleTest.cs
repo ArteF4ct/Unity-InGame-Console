629f7a3cee95a6717f676a3d0e9df607af05fe84
Musa Özey <musaozey@Musas-MacBook-Pro.local>
2017-07-09 12:53:39 +0300

    Added parameter control tests

--- Assets/MConsoleTest.cs
+++ Assets/MConsoleTest.cs
@@ -7,14 +7,29 @@ public class MConsoleTest : MonoBehaviour
 {
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.S))
+		if (Input.GetKeyDown(KeyCode.A))
 		{
 			MConsole.MConsole.Instance().ExecuteCommand("close_console");
 		}
 
-		if (Input.GetKeyDown(KeyCode.D))
+		if (Input.GetKeyDown(KeyCode.S))
 		{
 			MConsole.MConsole.Instance().ExecuteCommand("example gg 1 aaaa");
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			MConsole.MConsole.Instance().ExecuteCommand("close_console test");
+		}
+
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			MConsole.MConsole.Instance().ExecuteCommand("example gg 1");
+		}
+
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			MConsole.MConsole.Instance().ExecuteCommand("example gg 1 aaa test");
 		}
 	}
 }
